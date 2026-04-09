0c2a71be29d9f6e441d53521708a8591e9584e8d
GrizzlyTycoon <grizzlytycoon@gmail.com>
2026-04-09 21:27:29 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+
+app.post('/tap', (req, res) => {
+  res.json({ message: "Tap received!" });
+});
+
+app.get('/', (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
