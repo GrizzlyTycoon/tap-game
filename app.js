430fb958ce5ec2572ee8b0fb70b03307f662ea1e
GrizzlyTycoon <grizzlytycoon@gmail.com>
2026-04-12 19:53:44 +0530

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let coins = 0;
+
 async function tap() {
     try {
         await fetch('https://tap-game-5271.onrender.com', {
@@ -9,4 +11,11 @@ async function tap() {
 
     coins++;
     document.getElementById('coins').innerText = coins;
+}
+
+if (window.Telegram && Telegram.WebApp) {
+    const user = Telegram.WebApp.initDataUnsafe.user;
+    if (user) {
+        console.log("User ID:", user.id);
+    }
 }
