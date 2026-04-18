28892204c9f8d528cda93729cdb743bf2f286eb1
GrizzlyTycoon <grizzlytycoon@gmail.com>
2026-04-18 10:12:42 +0530

    app.js

--- app.js
+++ app.js
@@ -30,3 +30,25 @@ window.tap = async function () {
     console.log("ERROR:", e);
   }
 };
+
+window.onload = async function () {
+  try {
+    const res = await fetch('https://tap-game-5271.onrender.com/user', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userId: Telegram.WebApp.initDataUnsafe.user.id
+      })
+    });
+
+    const data = await res.json();
+
+    coins = data.coins || 0;
+
+    const el = document.getElementById('coins');
+    if (el) el.innerText = coins;
+
+  } catch (e) {
+    console.log("LOAD ERROR:", e);
+  }
+};
